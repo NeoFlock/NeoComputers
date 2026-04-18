93a4c4318f2ff291ee4ab55be0648366d106a614
IonutParau <ionut.alex.parau@gmail.com>
2026-04-18 12:00:42 +0200

    made the particles better

--- src/main/kotlin/org/neoflock/neocomputers/block/Generators.kt
+++ src/main/kotlin/org/neoflock/neocomputers/block/Generators.kt
@@ -3,29 +3,20 @@ package org.neoflock.neocomputers.block
 import dev.architectury.registry.menu.MenuRegistry
 import net.minecraft.core.BlockPos
 import net.minecraft.core.particles.ParticleTypes
-import net.minecraft.network.chat.ChatType
-import net.minecraft.network.chat.OutgoingChatMessage
-import net.minecraft.network.chat.PlayerChatMessage
-import net.minecraft.server.level.ServerLevel
 import net.minecraft.server.level.ServerPlayer
-import net.minecraft.sounds.SoundEvent
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.sounds.SoundSource
 import net.minecraft.util.RandomSource
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.EntityBlock
-import net.minecraft.world.level.block.FurnaceBlock
 import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BooleanProperty
-import net.minecraft.world.level.storage.loot.LootParams
 import net.minecraft.world.phys.BlockHitResult
 import org.neoflock.neocomputers.entity.BlockEntities
 import org.neoflock.neocomputers.entity.SolarGeneratorBlockEntity
@@ -79,7 +70,8 @@ class CombustionGeneratorBlock : NodeBlock, EntityBlock {
             val y = blockPos.y.toDouble()
             val z = blockPos.z.toDouble()
 
-            level.addParticle(ParticleTypes.SMOKE, x+0.5, y+0.5, z+0.5, 0.0, 0.0, 0.0)
+            level.addParticle(ParticleTypes.SMOKE, x+0.5, y+1, z+0.5, 0.0, 0.0, 0.0)
+            level.addParticle(ParticleTypes.FLAME, x+0.5, y+1, z+0.5, 0.0, 0.0, 0.0)
         }
     }
 }
